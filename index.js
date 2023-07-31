0b421531e8db936e13bc4f00b5e626ff2a3246ef
Loockxy <81113543+Loockxy@users.noreply.github.com>
2023-08-01 00:36:01 +0300

    Finish

---
+++ index.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const intents = new Discord.IntentsBitField(3276799)
+const client = new Discord.Client({intents});
+const config = {
+	token: "token du bot",
+	owner: "ton id"
+}
+
+const consoleCmd = new Discord.SlashCommandBuilder().setName("console").setDescription("Cette commande vous permez d'éxécuter une commande sur votre console de votre serveur FiveM").setStringOption(options => options.setName("commande").setDescription("La commande que vous voulez éxécuter sur votre serveur FiveM".setRequired(true)).setDMPermission(true)
+
+client.on("ready", () => {
+	console.log(`${client.user.tag} est connecter`)
+	
+	client.application.command.create(consoleCmd)
+})
+
+client.on("interactionCreate", interaction =>{
+   if(!interaction.isCommand) return;
+   if(interaction.user.id !== config.owner) return;
+   
+   if(interaction.commandName === "console") {
+        const cmd = interaction.options.getString("commande")
+        
+        ExecuteCommand(cmd)
+        
+        interaction.reply("La commande a bien été envoyer et éxécuter dans la console de votre serveur FiveM")
+   }
+}
+
+client.login(config.token)
