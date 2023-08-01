985df1678dccb0a5d622c7a5dba2d231e918c8b6
Loockxy <81113543+Loockxy@users.noreply.github.com>
2023-08-01 10:00:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const intents = new Discord.IntentsBitField(3276799)
 const client = new Discord.Client({intents});
 const config = require('config.json');
 
-const consoleCmd = new Discord.SlashCommandBuilder().setName("console").setDescription("Cette commande vous permez d'éxécuter une commande sur votre console de votre serveur FiveM").setStringOption(options => options.setName("commande").setDescription("La commande que vous voulez éxécuter sur votre serveur FiveM".setRequired(true)).setDMPermission(true)
+const consoleCmd = new Discord.SlashCommandBuilder().setName("console").setDescription("Cette commande vous permez d'éxécuter une commande sur votre console de votre serveur FiveM").setStringOption(options => options.setName("commande").setDescription("La commande que vous voulez éxécuter sur votre serveur FiveM").setRequired(true)).setDMPermission(true)
 
 client.on("ready", () => {
 	console.log(`${client.user.tag} est connecter`)
