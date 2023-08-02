41f2213854ee0fe17d266bff8e4d9be3b26c13d5
Loockxy <81113543+Loockxy@users.noreply.github.com>
2023-08-03 00:39:12 +0300

    Bug Fix

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 const Discord = require('discord.js');
 const intents = new Discord.IntentsBitField(3276799)
 const client = new Discord.Client({intents});
-const config = require('config.json');
+const config = require('./config.json');
 
-const consoleCmd = new Discord.SlashCommandBuilder().setName("console").setDescription("Cette commande vous permez d'éxécuter une commande sur votre console de votre serveur FiveM").setStringOption(options => options.setName("commande").setDescription("La commande que vous voulez éxécuter sur votre serveur FiveM").setRequired(true)).setDMPermission(true)
+const consoleCmd = new Discord.SlashCommandBuilder()
+.setName("console")
+.setDescription("Cette commande vous permez d'éxécuter une commande sur votre console de votre serveur FiveM")
+.setStringOption(options =>
+    options.setName("commande")
+    .setDescription("La commande que vous voulez éxécuter sur votre serveur FiveM")
+    .setRequired(true)
+)
+.setDMPermission(true);
 
 client.on("ready", () => {
-	console.log(`${client.user.tag} est connecter`)
-	
-	client.application.command.create(consoleCmd)
+    console.log(`${client.user.tag} est connecter`)
+                	
+    client.application.command.create(consoleCmd)
 })
 
-client.on("interactionCreate", interaction =>{
-   if(!interaction.isCommand) return;
-   if(interaction.user.id !== config.owner) return;
-   
-   if(interaction.commandName === "console") {
+client.on("interactionCreate", interaction => {
+    if(!interaction.isCommand) return;
+    if(interaction.user.id !== config.owner) return;
+                                 
+    if(interaction.commandName === "console") {
         const cmd = interaction.options.getString("commande")
-        
+                                                    
         ExecuteCommand(cmd)
-        
+                                                                    
         interaction.reply("La commande a bien été envoyer et éxécuter dans la console de votre serveur FiveM")
-   }
-}
+    }
+})
 
 client.login(config.token)
